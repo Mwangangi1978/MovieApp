7f1f2265deb400aa394a2e76728bebf86469a52a
Denis Mwangangi <dennomwangs1978@gmail.com>
2024-01-17 20:57:35 +0300

    API added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+const API_URL = 'http://www.omdbapi.com?apikey=813042b1' 
 
 function App() {
+  const searchMovies = async (title)=>{
+    const response = await fetch (`${API_URL}&s=${title} `)
+    const data = await response.json();
+    console.log(data)
+  }
   
-
+  useEffect(()=>{
+    searchMovies('spiderman')
+  }, [])
+   
   return (
     <>
       <h1>Movie App</h1>
